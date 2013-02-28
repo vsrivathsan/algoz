bfa1b25c96e323e1d2f937981fc719e106ea17a5
vsrivathsan <sudheesh88@gmail.com>
2013-02-27 21:14:19 -0800

    Insertion Sort and Quick Sort pseudo code

---
+++ src/edu/master_sri/algoz/sort/InsertionSort.java
@@ -0,0 +1,13 @@
+package edu.master_sri.algoz.sort;
+
+public class InsertionSort {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/edu/master_sri/algoz/sort/QuickSort.java
@@ -0,0 +1,13 @@
+package edu.master_sri.algoz.sort;
+
+public class QuickSort {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
